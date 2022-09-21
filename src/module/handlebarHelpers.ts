3cc3cb2e1e7b461bc62610426777a6adbd7b6f76
Pyrobow <DunlopW@outlook.com>
2022-09-21 11:04:47 +0100

    FEATURE-004 Second temporary commit to allow remote working.

--- src/module/handlebarHelpers.ts
+++ src/module/handlebarHelpers.ts
@@ -2,4 +2,8 @@ export default function registerHelpers(): void {
   Handlebars.registerHelper('subtract', function (a, b) {
     return a - b;
   });
+
+  Handlebars.registerHelper('concat', function (a: string, b: string) {
+    return `${a}${b}`;
+  });
 }
